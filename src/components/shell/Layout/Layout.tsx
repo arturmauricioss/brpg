efb90edb527f0f59e3272223d76558dc1cb90323
Artur Maurício <arturmauricioss@outlook.com>
2026-05-18 11:48:22 -0300

    Removendo menu de navegação nas páginas não logadas

--- src/components/shell/Layout/Layout.tsx
+++ src/components/shell/Layout/Layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import Box from '@components/ui/basic/Box/Box';
 import PageNavigation from '@features/pageNavigation/components/PageNavigation';
+import { useAuth } from '@hooks/useAuth';
 import './Layout.css';
 
 export default function Layout() {
+  const { user } = useAuth();
+
   return (
     <Box className="app-layout">
       <main className="app-main">
         <Outlet />
       </main>
-      <PageNavigation />
+
+      {user && <PageNavigation />}
     </Box>
   );
 }
